e9ab41dfa60923a8dea1b20cfcabea382281904d
github username <github email address>
2020-02-17 06:25:55 -0800

    first commit

---
+++ lib/puppy_finder.rb
@@ -0,0 +1,6 @@
+require "puppy_finder/version"
+
+module PuppyFinder
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/puppy_finder/version.rb
@@ -0,0 +1,3 @@
+module PuppyFinder
+  VERSION = "0.1.0"
+end
